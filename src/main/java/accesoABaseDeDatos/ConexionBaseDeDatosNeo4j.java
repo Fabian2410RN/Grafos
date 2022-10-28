2eb38b39351f7f61f395c74405c576e2ebbd678b
Fabian2410RN <fabianriosig26@gmail.com>
2022-10-27 23:15:29 -0600

    Fabian

---
+++ src/main/java/accesoABaseDeDatos/ConexionBaseDeDatosNeo4j.java
@@ -0,0 +1,43 @@
+
+
+package accesoABaseDeDatos;
+
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Result;
+import org.neo4j.driver.Session;
+
+import static org.neo4j.driver.Values.parameters;
+
+public class ConexionBaseDeDatosNeo4j implements AutoCloseable {
+    private final Driver driver;
+    
+    public ConexionBaseDeDatosNeo4j(String uri, String user, String password) {
+        driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
+    }
+
+    @Override
+    public void close() throws RuntimeException {
+        driver.close();
+    }
+
+    @SuppressWarnings("deprecation")
+    public void cargarCSVs(final String message) {
+        try (Session session = driver.session()) {
+        session.run("LOAD CSV WITH HEADERS FROM 'file:///C:/Users/Estadm/Downloads/CSV/Clientes.csv' AS clientes create (c1:Cliente {id: clientes.id, first_name: clientes.first_name, last_name: clientes.last_name})");
+        session.run("LOAD CSV WITH HEADERS FROM 'file:///C:/Users/Estadm/Downloads/CSV/Compras.csv' AS compras create (c2:Compra {idCliente: compras.idCliente, idProducto: compras.idProducto, cantidad: compras.cantidad})");
+        session.run("LOAD CSV WITH HEADERS FROM 'file:///C:/Users/Estadm/Downloads/CSV/Marcas.csv' AS marcas create (c3:Marca {id: marcas.id, nombre: marcas.nombre, pais: marcas.pais})");
+        session.run("LOAD CSV WITH HEADERS FROM 'file:///C:/Users/Estadm/Downloads/CSV/Productos.csv' AS productos create (c4:Producto {id: productos.id, nombre: productos.nombre, marca: productos.marca, precio: productos.precio})");
+  
+        }
+    }
+    /*
+    public static void main(String... args) throws Exception {
+        try (HelloWorldExample greeter = new HelloWorldExample("bolt://localhost:7687", "neo4j", "1234")) {
+            greeter.printGreeting("hello, world");
+        }
+    }*/
+}
+
+
--- src/main/java/controladorWeb/CControladorCargaCSVs.java
+++ src/main/java/controladorWeb/CControladorCargaCSVs.java
@@ -1,8 +1,7 @@
 package controladorWeb;
 
 
-import accesoABaseDeDatos.HelloWorldExample;
-import accesoABaseDeDatos.HelloWorldExample;
+import accesoABaseDeDatos.ConexionBaseDeDatosNeo4j;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -25,32 +24,10 @@ public class CControladorCargaCSVs extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         System.out.println("Entrando Do get");
-        //ConexionBaseNeo4j conexion1 = new ConexionBaseNeo4j();
-        //Connection bd = conexion1.conexion();
-        HelloWorldExample greeter = new HelloWorldExample("bolt://localhost:7687", "neo4j", "1234");
-        greeter.printGreeting("Hola");
+        ConexionBaseDeDatosNeo4j greeter = new ConexionBaseDeDatosNeo4j("bolt://localhost:7687", "neo4j", "1234");
+        greeter.cargarCSVs("Hola");
         System.out.println("Hecho");
-        /*
-        String cargarCSVClientes;
-        String cargarCSVCompras;
-        String cargarCSVMarcas;
-        String cargarCSVProductos;
-        //nuevoNodo = "Create(c:Cliente{id: '33', first_name: 'Carlos', last_name: '"+nombre+"'})";
         
-        try{
-            PreparedStatement csvClientes = bd.prepareStatement(cargarCSVClientes);
-            PreparedStatement csvCompras = bd.prepareStatement(cargarCSVCompras);
-            PreparedStatement csvMarcas = bd.prepareStatement(cargarCSVMarcas);
-            PreparedStatement csvProductos = bd.prepareStatement(cargarCSVProductos);
-            
-            csvClientes.executeUpdate();
-            csvCompras.executeUpdate();
-            csvMarcas.executeUpdate();
-            csvProductos.executeUpdate();
-            
-        }catch(SQLException ex){
-            System.out.println("Error");
-        }*/
         request.getRequestDispatcher("CargarCSVs.jsp").forward(request, response);
     }
     
